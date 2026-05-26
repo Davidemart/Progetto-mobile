1cfa28af901534cf4afafd29785e1b9af68ac55b
Davidemart1 <davidemartelli2004@gmail.com>
2026-05-26 13:42:25 +0200

    app.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import 'react-native-get-random-values';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+    </>
+  );
+}
