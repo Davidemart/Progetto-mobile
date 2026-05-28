d6dd87a8b3f2d3ffa08ee4cc39fd4caf2e4e322c
Davidemart1 <davidemartelli2004@gmail.com>
2026-05-28 14:46:59 +0200

    Revert "Aggiunte schermate RecipeList e MealPlan e aggiornati i relativi navigatori"
    This reverts commit 066e5ad03a9ef29f50592c228121a751825dd128.

--- src/navigations/RecipeNavigator.tsx
+++ src/navigations/RecipeNavigator.tsx
@@ -1,7 +1,7 @@
-import react from "react";
+import React from "react";
 
 export const RecipeNavigator = () => {
-    return (
-        <></>
-    );
+  return (
+    <></>
+  );
 }
--- src/screens/MealPlan/MealPlanScreen.tsx
+++
@@ -1,7 +0,0 @@
-import react from "react";
-
-export const MealPlanScreen = () => {
-  return (
-    <></>
-  );
-}
